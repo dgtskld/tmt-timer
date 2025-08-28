bebbf944e5da0ec72ae13e4edaa9b26a95568874
dgtskld <mazunin.dev@gmail.com>
2025-08-28 23:53:52 +0400

    Initial commit

---
+++ tmt/app/tmtApp.swift
@@ -0,0 +1,14 @@
+//
+//  Creating amazing apps has never been easier.
+//
+
+import SwiftUI
+
+@main
+struct tmtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tmt_tests/tmtTests.swift
@@ -0,0 +1,13 @@
+//
+//  Creating amazing apps has never been easier.
+//
+
+import Testing
+
+struct tmtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
